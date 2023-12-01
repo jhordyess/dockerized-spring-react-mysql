6907cfc9eb3ddcd19e4718db0a5c3e0bedd94305
jhordyess <dev@jhordyess.com>
2023-12-01 10:52:53 -0400

    Added code generated by Spring Initializr - Project: Maven - Language: Java - Spring Boot: 3.2.0 - Packaging: Jar - Java: 17 - Dependencies:    - Spring Web    - MySQL Driver    - Spring Data JPA    - Spring Boot DevTools

---
+++ backend/src/test/java/com/jhordyess/demouser/DemoUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhordyess.demouser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
